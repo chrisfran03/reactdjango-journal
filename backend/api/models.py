a75cbb9fb5669914e2c9bf43d131436240eab08f
chrisfran03 <francischristo03@gmail.com>
2025-03-01 15:51:30 +1300

    add serializer for model note serializer.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Notes(models.Model):
+class Note(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     # auto_now_add repopulates the field wheneer we make a new version or new instance of te note object
