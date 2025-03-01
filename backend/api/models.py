519e053c6787418ff9c708dece1b3ca8c5b12639
chrisfran03 <francischristo03@gmail.com>
2025-03-01 15:27:44 +1300

    add model for notes

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Notes(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    # auto_now_add repopulates the field wheneer we make a new version or new instance of te note object
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        # user will have a one to many relationship with notes having the foreign key to link to the user field
+        User, on_delete=models.CASCADE, related_name="notes")
+
+    def __str__(self):
+        return self.title
