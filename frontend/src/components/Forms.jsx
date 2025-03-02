1507a56ec9f5ea4c7264d625dc47e51a94b0dab2
chrisfran03 <francischristo03@gmail.com>
2025-03-02 22:03:02 +1300

    add form styles

--- frontend/src/components/Forms.jsx
+++ frontend/src/components/Forms.jsx
@@ -8,9 +8,28 @@ function Form({ route, method }) {
     const [password, setPassword] = useState("")
     const [loading, setLoading] = useState(false)
     const navigate = useNavigate()
+
     const name = method === "login" ? "Login" : "Register"
-    const handleSubmit = (e) => {
-        e.preventDefaullt()
+
+    const handleSubmit = async (e) => {
+        setLoading(true);
+        e.preventDefaullt();
+
+        try {
+            const res = await api.post(route, { username, password })
+            if (method === "login") {
+                localStorage.setItem(ACCESS_TOKEN, res.data.access);
+                localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+                navigate("/")
+            } else {
+                navigate("/login")
+            }
+        }
+        catch (error) {
+            alert(error)
+        } finally {
+            setLoading(false)
+        }
     }
 
     return <form onSubmit={handleSubmit} className="form-container">
